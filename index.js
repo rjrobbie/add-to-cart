854eaa8890c2441fe6be1eb17eb92d8cac836e44
Rob B <rob@rjrobbie.dev>
2023-04-15 11:47:25 +0100

    create HTML, CSS and JS files

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputEl = document.getElementById("input-field")
+const addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", () => {
+    let inputValue = inputEl.value;
+    console.log(inputValue);
+})
