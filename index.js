23b12cdbca82b6f1ed5b458cf81b96e8d49c73cd
Rob B <rob@rjrobbie.dev>
2023-04-15 20:17:30 +0100

    link app to Firebase database

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://add-to-cart-b362e-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const groceryItemInDB = ref(database, "groceries");
+
+
 const inputEl = document.getElementById("input-field")
 const addButton = document.getElementById("add-button");
 
 addButton.addEventListener("click", () => {
     let inputValue = inputEl.value;
-    console.log(inputValue);
+
+    push(groceryItemInDB, inputValue)
+    console.log(`${inputValue} added to database`);
 })
