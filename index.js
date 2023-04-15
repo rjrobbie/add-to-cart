311dff150ed3f05f2d590d783348c1b63d1a4ae5
Rob B <rob@rjrobbie.dev>
2023-04-15 22:32:13 +0100

    debugged array appending to li element when a new item is added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://add-to-cart-b362e-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -7,15 +7,50 @@ const appSettings = {
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-const groceryItemInDB = ref(database, "groceries");
-
+const shoppingList = ref(database, "shoppingList");
 
 const inputEl = document.getElementById("input-field")
 const addButton = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButton.addEventListener("click", () => {
     let inputValue = inputEl.value;
 
-    push(groceryItemInDB, inputValue)
-    console.log(`${inputValue} added to database`);
+    if (!inputValue.trim()) {
+        alert("Nothing to add");
+        return;
+    }
+
+    push(shoppingList, inputValue)
+
+    clearInput();
+
 })
+
+onValue(shoppingList, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    
+    clearShoppingList()
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        addNewItem(itemsArray[i])
+    }
+})
+
+const clearShoppingList = () => {
+    shoppingListEl.innerHTML = ""
+}
+
+const clearInput = () => {
+    inputEl.value = "";
+};
+
+const addNewItem = (itemValue) => {
+    const existingItems = Array.from(shoppingListEl.children).map((li) => li.textContent.trim());
+    
+    if (existingItems.includes(itemValue)) {
+        return;
+    }
+    
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+};
